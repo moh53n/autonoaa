0300388af824f0c26e5caed79862e9faee99f944
Mohsen Tahmasebi <moh53n@outlook.com>
2022-06-05 11:33:24 +0430

    Update Satellite object

--- autonoaa/core/Satellite.py
+++ autonoaa/core/Satellite.py
@@ -3,6 +3,8 @@ class Satellite:
     Satellite class
     """
 
+    id = None
+    catnr = None
     name = None
     type_name = None
     tle = None
@@ -10,7 +12,9 @@ class Satellite:
     service = None
     bandwidth = None
 
-    def __init__(self, name: str, type_name: str, tle: str, frequency: int, bandwidth: int, service: str):
+    def __init__(self, id: int, catnr: int, name: str, type_name: str, tle: str, frequency: int, bandwidth: int, service: str):
+        self.id = id
+        self.catnr = catnr
         self.name = name
         self.type_name = type_name
         self.tle = tle
