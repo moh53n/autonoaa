546af46e27409a929faa778319a66c09b8e195d5
Mohsen Tahmasebi <moh53n@moh53n.ir>
2023-06-30 23:03:07 +0330

    check if the pass is expired

--- autonoaa/Recorder/capture.py
+++ autonoaa/Recorder/capture.py
@@ -5,6 +5,7 @@ from autonoaa.Core import DB
 import subprocess
 from autonoaa.satdump import satdump
 from crontab import CronTab
+from dateutil import tz
 
 METEOR_COMMAND = """timeout "{CAPTURE_TIME}" rtl_fm -M raw -f "{freq}" -p "{FREQ_OFFSET}" -s {BANDWIDTH} -g "{GAIN}" | sox -t raw -r {BANDWIDTH} -c 2 -b 16 -e signed - -t wav "{OUT_FILE}" rate 96k"""
 NOAA_COMMAND = """timeout "{CAPTURE_TIME}" rtl_fm -f "{freq}" -p "{FREQ_OFFSET}" -s {BANDWIDTH} -g "{GAIN}" -E wav -E deemp -F 9 - | sox -t raw -e signed -c 1 -b 16 -r {BANDWIDTH} - "{OUT_FILE}" rate 11025"""
@@ -31,7 +32,6 @@ def run(device_conf, satellite, duration: int):
     rec(id, device_conf, satellite, duration)
     satdump(id, satellite, f"{id}.wav")
 
-#TODO: check if the pass is ended
 def capture(pass_id, device):
     pass_ = DB.get_pass(pass_id)
 
@@ -51,6 +51,10 @@ def capture(pass_id, device):
 
     start = datetime.datetime.strptime(pass_[0].pass_start, "%Y-%m-%d %H:%M:%S.%f%z")
     end = datetime.datetime.strptime(pass_[0].pass_end, "%Y-%m-%d %H:%M:%S.%f%z")
+
+    if datetime.datetime.now().replace(tzinfo=tz.tzlocal()) > end:
+        print("Pass time expired")
+        return False
 
     duration = (end - start).total_seconds()
 
